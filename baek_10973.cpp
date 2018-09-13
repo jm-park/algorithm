8d9ceb044b7f85be6a6e16e8883023c98cf023e4
jm-park <jmpark7559@gmail.com>
2018-09-13 22:09:33 +0900

    baek_10973

---
+++ baek_10973.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, tmp;
+	vector<int> nums;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &tmp);
+		nums.push_back(tmp);
+	}
+	
+	if (prev_permutation(nums.begin(), nums.end()))
+		for (int i = 0; i < n; i++)
+			printf("%d ", nums[i]);
+	else 
+		printf("-1\n");
+
+	return 0;
+}
